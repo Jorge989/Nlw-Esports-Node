7011eba2b6337f5cab0fee17a8ce84d7142545a1
Jorge Attie <attiejorge89@gmail.com>
2022-09-14 08:48:24 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+const app = express();
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  const gameId = request.params.id;
+
+  return response.json([
+    { id: 1, name: "Anuncio 1" },
+    { id: 2, name: "Anuncio 2" },
+    { id: 3, name: "Anuncio 3" },
+    { id: 4, name: "Anuncio 4" },
+    { id: 5, name: "Anuncio 5" },
+  ]);
+});
+app.get("/ads/:id/ads", (request, response) => {
+  const adId = request.params.id;
+
+  return response.json([]);
+});
+app.get("/ads/:id/discord", (request, response) => {
+  const adId = request.params.id;
+
+  return response.json([]);
+});
+app.listen(3333);
